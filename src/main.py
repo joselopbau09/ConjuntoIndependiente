18af08ff948bae6f769a2881674c1571a35039db
Jose Luis Bautista <joseluis27@ciencias.unam.mx>
2022-09-25 20:49:12 -0500

    Lectura de archivo agregado

--- src/main.py
+++ src/main.py
@@ -1,7 +1,34 @@
 
-def lecturaArchivo(nombre):
-    listaLIneas = []
-    with open(nombre) as archivo:
-        listaLIneas = archivo.readlines()
+from Graph import Graph
 
-    
+def lecturaArchivo(nombre):
+    listaLineas = []
+    with open(nombre) as archivo:
+        listaLineas = archivo.readlines()
+    grafica = creacionGrafica(listaLineas.pop(0))
+    return unirVertices(listaLineas, grafica)
+
+def creacionGrafica(cadena):
+    grafica = Graph()
+    listaVertices  = cadena.split(',')
+    eliminarSalto = listaVertices[len(listaVertices) - 1].replace('\n','')
+    listaVertices[len(listaVertices) - 1] = eliminarSalto
+    for vertice in listaVertices:
+        grafica.addVertex(vertice)
+            
+    return grafica
+
+def unirVertices(listaCadenas, grafica):
+    graph = grafica
+    for cadena in listaCadenas:
+        listaAristas  = cadena.split(',')
+        eliminarSalto = listaAristas[1].replace('\n','')
+        listaAristas[1] = eliminarSalto
+        graph.addEdge(listaAristas[0], listaAristas[1])
+    return graph    
+
+def main():
+
+    lecturaArchivo('graph.txt')
+
+main()    
